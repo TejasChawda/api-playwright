c6a6d3d479038d912dbecaae91ade70858dd64cb
tejas <tejasc@testvagrant.com>
2026-06-11 16:16:51 +0530

    info added about ajv

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -2,6 +2,9 @@ import { Ajv } from 'ajv';
 
 const ajv = new Ajv();
 
+//Note - if u want to validate formats, then you need to add the ajv-formats package and 
+//register it with Ajv instance
+
 export function validateSchema(data: any, schema: object): boolean {
     const validate = ajv.compile(schema);
     const valid = validate(data);
